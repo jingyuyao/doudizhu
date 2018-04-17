da5a484d6b5b9c11f75774c68aa64c7749e044bb
Jingyu Yao <yjy.emails@gmail.com>
2018-04-17 16:40:13 -0400

    woops

--- src/main/scala/doudizhu/DumbAgent.scala
+++ src/main/scala/doudizhu/DumbAgent.scala
@@ -4,9 +4,7 @@ package doudizhu
   * A stupid agent that always accepts landlord and always try to beat the last play with the
   * weakest combo possible.
   */
-class DumbAgent(override val id: PlayerId,
-                override val secret: PlayerSecret)
-  extends Agent(id, secret) {
+class DumbAgent(id: PlayerId, secret: PlayerSecret) extends Agent(id, secret) {
   /** Returns whether to become the landlord. */
   override def getAction(auctionState: AuctionState): Boolean = true
 
