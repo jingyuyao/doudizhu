60af000c960aa77cc93ce752c8b48e65e500a4cf
Jingyu Yao <yjy.emails@gmail.com>
2018-04-18 10:38:14 -0400

    fix expectimax algorithm bug.
    i cant even copy my own code correctly fml

--- src/main/scala/doudizhu/SmartAgent.scala
+++ src/main/scala/doudizhu/SmartAgent.scala
@@ -12,22 +12,23 @@ class SmartAgent(agentId: AgentId, agentSecret: AgentSecret, maxDepth: Int = 1) 
   /** Returns the play to make, None to pass. */
   override def getAction(playingState: PlayingState): Option[Combo] = {
     val fakePlayingState = playingState.toFake(agentSecret)
-    val successors =
-      getValidCombos(fakePlayingState, agentId)
-        .map(combo => (combo, fakePlayingState.play(agentId, combo)))
-    if (successors.isEmpty) {
-      None
-    } else {
+    val comboSuccessorPairs =
+      getValidCombos(fakePlayingState, agentId).map(combo => (combo, fakePlayingState.play(agentId, combo)))
+    if (comboSuccessorPairs.nonEmpty) {
       val otherAgents = getOtherAgentsInOrder(fakePlayingState)
-      val comboValues = successors.map({ case (combo, state) => (combo, minValue(state, 0, otherAgents)) })
+      val comboValues = comboSuccessorPairs.map({ case (combo, state) => (combo, minValue(state, 0, otherAgents)) })
       Some(comboValues.maxBy(_._2)._1)
+    } else {
+      None
     }
   }
 
   private def maxValue(fakePlayingState: FakePlayingState, currentDepth: Int): Double =
     if (isTerminal(fakePlayingState, currentDepth)) {
+      println(f"max $currentDepth terminal ")
       eval(fakePlayingState)
     } else {
+      println(f"max $currentDepth")
       val successorStates = getSuccessorStates(fakePlayingState, agentId)
       val otherAgents = getOtherAgentsInOrder(fakePlayingState)
       val minValues = successorStates.map(state => minValue(state, currentDepth, otherAgents)) :+ Double.NegativeInfinity
@@ -36,16 +37,16 @@ class SmartAgent(agentId: AgentId, agentSecret: AgentSecret, maxDepth: Int = 1) 
 
   private def minValue(fakePlayingState: FakePlayingState, currentDepth: Int, otherAgents: List[AgentId]): Double =
     if (isTerminal(fakePlayingState, currentDepth)) {
+      println(f"min $currentDepth terminal ")
       eval(fakePlayingState)
     } else {
+      println(f"min $currentDepth")
+      val successorStates = getSuccessorStates(fakePlayingState, otherAgents.head)
       val maxValues =
-        if (otherAgents.isEmpty) {
-          getSuccessorStates(fakePlayingState, agentId)
-            .map(state => maxValue(state, currentDepth + 1))
-        } else {
-          getSuccessorStates(fakePlayingState, otherAgents.head)
-            .map(state => minValue(state, currentDepth, otherAgents.drop(1)))
-        }
+        if (otherAgents.lengthCompare(1) == 0)
+          successorStates.map(state => maxValue(state, currentDepth + 1))
+        else
+          successorStates.map(state => minValue(state, currentDepth, otherAgents.drop(1)))
       if (maxValues.isEmpty)
         Double.PositiveInfinity
       else
@@ -58,14 +59,14 @@ class SmartAgent(agentId: AgentId, agentSecret: AgentSecret, maxDepth: Int = 1) 
   private def getSuccessorStates(fakePlayingState: FakePlayingState, id: AgentId): List[FakePlayingState] =
     getValidCombos(fakePlayingState, id).map(combo => fakePlayingState.play(id, combo))
 
-  private def getValidCombos(fakePlayingState: FakePlayingState, id: AgentId): List[Combo] =
-    if (id == agentId) {
-      val hand = fakePlayingState.getHand(agentSecret)
-      Combo.allFrom(hand).filter(combo => fakePlayingState.isValid(id, combo))
-    } else {
-      val otherCardsInPlay = fakePlayingState.otherCardsInPlay(agentSecret)
-      Combo.allFrom(otherCardsInPlay).filter(combo => fakePlayingState.isValid(id, combo))
-    }
+  private def getValidCombos(fakePlayingState: FakePlayingState, id: AgentId): List[Combo] = {
+    val availableCards =
+      if (id == agentId)
+        fakePlayingState.getHand(agentSecret)
+      else
+        fakePlayingState.otherCardsInPlay(agentSecret)
+    Combo.allFrom(availableCards).filter(combo => fakePlayingState.isValid(id, combo))
+  }
 
   private def getOtherAgentsInOrder(fakePlayingState: FakePlayingState): List[AgentId] = {
     val allAgentIds = fakePlayingState.getAllAgentIds
