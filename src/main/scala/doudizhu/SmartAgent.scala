9a1d4f8f705c45b4a737aa53f256f75f4d2d0b46
Jingyu Yao <yjy.emails@gmail.com>
2018-04-20 01:22:27 -0400

    variable expectimax

--- src/main/scala/doudizhu/SmartAgent.scala
+++ src/main/scala/doudizhu/SmartAgent.scala
@@ -48,8 +48,8 @@ class SmartAgent(agentId: AgentId,
       getSmartActions(fakePlayingState, agentId).map(combo => (combo, fakePlayingState.play(agentId, combo)))
     val result =
       if (comboSuccessorPairs.nonEmpty) {
-        val otherAgents = getOtherAgentsInOrder(fakePlayingState)
-        val comboValues = comboSuccessorPairs.map({ case (combo, state) => (combo, minValue(state, 0, otherAgents)) })
+        val nextAgent = getNextAgent(fakePlayingState, agentId)
+        val comboValues = comboSuccessorPairs.map({ case (combo, state) => (combo, expectiMax(state, 0, nextAgent)) })
         val maxComboValue = comboValues.maxBy(_._2)
         val currentStateValue = eval(fakePlayingState)
         if (DEBUG) println(f"    current state $currentStateValue, max combo $maxComboValue")
@@ -68,41 +68,37 @@ class SmartAgent(agentId: AgentId,
     result
   }
 
-  private def maxValue(fakePlayingState: FakePlayingState, currentDepth: Int): Double =
+  private def expectiMax(fakePlayingState: FakePlayingState, currentDepth: Int, currentAgent: AgentId): Double =
     if (isTerminal(fakePlayingState, currentDepth)) {
       eval(fakePlayingState)
     } else {
-      val successorStates = getSuccessorStates(fakePlayingState, agentId)
-      val otherAgents = getOtherAgentsInOrder(fakePlayingState)
-      val minValues = successorStates.map(state => minValue(state, currentDepth, otherAgents))
-      minValues.max
-    }
-
-  private def minValue(fakePlayingState: FakePlayingState, currentDepth: Int, otherAgents: List[AgentId]): Double =
-    if (isTerminal(fakePlayingState, currentDepth)) {
-      eval(fakePlayingState)
-    } else {
-      val successorStates = getSuccessorStates(fakePlayingState, otherAgents.head)
-      val maxValues =
-        if (otherAgents.lengthCompare(1) == 0) {
-          val nextDepth = currentDepth + 1
-          successorStates.map(state => maxValue(state, nextDepth))
-        }
-        else {
-          val remainingAgents = otherAgents.drop(1)
-          successorStates.map(state => minValue(state, currentDepth, remainingAgents))
-        }
-      maxValues.sum / maxValues.size
+      val successorStates = getSuccessorStates(fakePlayingState, currentAgent)
+      val nextAgent = getNextAgent(fakePlayingState, currentAgent)
+      if (isSameTeam(fakePlayingState, currentAgent)) {
+        val minValues = successorStates.map(state => expectiMax(state, currentDepth, nextAgent))
+        minValues.max
+      } else {
+        val maxValues =
+          if (isSameTeam(fakePlayingState, nextAgent)) {
+            val nextDepth = currentDepth + 1
+            successorStates.map(state => expectiMax(state, nextDepth, nextAgent))
+          }
+          else {
+            successorStates.map(state => expectiMax(state, currentDepth, nextAgent))
+          }
+        maxValues.sum / maxValues.size
+      }
     }
 
   private def isTerminal(fakePlayingState: FakePlayingState, currentDepth: Int): Boolean =
     currentDepth == maxDepth || fakePlayingState.getWinner.nonEmpty
 
-  private def getOtherAgentsInOrder(fakePlayingState: FakePlayingState): List[AgentId] = {
-    val allAgentIds = fakePlayingState.getAllAgentIds
-    val indexOfAgent = allAgentIds.indexOf(agentId)
-    // Assumes round-robin style.
-    allAgentIds.slice(indexOfAgent + 1, allAgentIds.size) ++ allAgentIds.slice(0, indexOfAgent)
+  private def isSameTeam(fakePlayingState: FakePlayingState, currentAgent: AgentId): Boolean =
+    currentAgent == agentId || (agentId != fakePlayingState.landlord && currentAgent != fakePlayingState.landlord)
+
+  private def getNextAgent(fakePlayingState: FakePlayingState, currentAgent: AgentId): AgentId = {
+    val agents = fakePlayingState.getAllAgentIds
+    agents((agents.indexWhere(_ == currentAgent) + 1) % agents.size)
   }
 
   private def getSuccessorStates(fakePlayingState: FakePlayingState, id: AgentId): ParIterable[FakePlayingState] = {
@@ -117,6 +113,8 @@ class SmartAgent(agentId: AgentId,
     val smartActions =
       if (agentId == id)
         legalActions
+      else if (isSameTeam(fakePlayingState, id))
+        List().par
       else
         legalActions.toList.sortBy(smartComboValue).take(maxExpectiLayerExpansion).par
 
